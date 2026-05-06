097b3a5d3e8712eb0c2c30e8e0e77f969203e872
Muhammad Asim <mazammasim@gmail.com>
2026-05-06 18:23:56 +0500

    utility: add cookie utility

---
+++ server/src/utils/cookie.js
@@ -0,0 +1,9 @@
+//cookie utility
+export const sendTokenCookie = (res, token) => {
+    res.cookie('token', token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'strict',
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+};
